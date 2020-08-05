397c700c2052dc4b3ae3fe819ef8a31acb24f5ce
Ramses <ramses.development@gmail.com>
2020-08-05 00:51:06 -0300

    setup inicial projeto

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Footer from "./components/Footer";
 
 function App() {
 	return (
-		<div style={{ background: "#414141" }}>
+		<div style={{ background: "#141414" }}>
 			<Menu />
 
 			<BannerMain
